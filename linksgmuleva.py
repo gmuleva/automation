2cb75e12de49de4ba55aff9f194fb7ac3dfb587e
poppinsahu <sumeetkumarsahu12@gmail.com>
2020-08-21 11:25:02 +0530

    links file

---
+++ linksgmuleva.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver=webdriver.Firefox()
+
+driver.get("http://facebook.com/")
+links=driver.find_elements(By.TAG_NAME,"a")
+print(len(links))
+
+for link in links:
+    print(link.text)
+driver.close()
