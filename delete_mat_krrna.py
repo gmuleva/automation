edca1a5876a2e7f29a18a2c9397c4428f50c03be
poppinsahu <sumeetkumarsahu12@gmail.com>
2020-08-22 08:43:24 +0530

    alert command

---
+++ delete_mat_krrna.py
@@ -0,0 +1,5 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+driver=webdriver.Firefox()
+driver.get('https://www.ivolunteer.in/volunteer-sign-up')
+driver.find_element_by_xpath('')
