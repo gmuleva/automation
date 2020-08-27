59c2a92afc2cfeefcf54bb2fc6a3b7f563a35cd9
poppinsahu <sumeetkumarsahu12@gmail.com>
2020-08-27 16:33:42 +0530

    restructured and created directory

---
+++ practice/relationoperators.py
@@ -0,0 +1,6 @@
+name=input("Enter Name")
+if name=="durga":
+    print("Hello durga good morning")
+else:
+    print("Hello guest good morning")
+print("How are you!!!")
--- relationoperators.py
+++
@@ -1,9 +0,0 @@
-a="durga"
-b="durga"
-print("a>b is ",a>b)
-print("a<b is ",a<b)
-print("a>=b is ",a>=b)
-print("a<=b is ",a<=b)
-
-a>b is False
-a<b is True
